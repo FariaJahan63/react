bdad9bd1602d700952fc6adcd30ee03d62a9d9a5
Faria Jahan <fariaofficial85@gmail.com>
2026-05-19 17:37:09 +0600

    jsx conventions

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,21 @@
-import Header from "./component/Header";
-import Hero from "./component/Hero";
-import ContactForm from './component/ContactForm';
-import Footer from './component/Footer';
 
-const app = () => {
+
+const App = () => {
   return (
     <div>
-<Header />
- <Hero />
- <ContactForm />
- <Footer />
-
-
+      <button style={{
+        backgroundColor: "blue",
+        color: "white",
+        padding: "10px 20px",
+        border: "none",
+        borderRadius: "5px",
+        cursor: "pointer"
+      }} 
+      
+      
+      onClick={() => alert("Button clicked!")}>Click me</button>
+      <img src="https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg"  style={{ marginLeft: "20px" }} />
     </div>
   );
-
 };
-export default app;
+export default App;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,18 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from "./App"
 
-import Header from './component/Header';
-import Hero from './component/Hero';
-import ContactForm from './component/ContactForm';
-import Footer from './component/Footer';
+
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
-    <Hero />
-    <ContactForm />
-    <Footer />
+    <App />
   </StrictMode>
 )
